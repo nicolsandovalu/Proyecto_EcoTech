0c429875650d4e45067e829047a68fc88fb1baf4
NathalyBerrios <118325270+NathalyBerrios@users.noreply.github.com>
2025-11-30 23:29:39 -0300

    Limpieza de archivos .DS_Store y carpetas __pycache__

--- src/dominio/cargo.py
+++
@@ -1,8 +0,0 @@
-class Cargo:
-    
-    def __init__(self, id_cargo: str, nombre: str):
-        self._id = id_cargo
-        self._nombre = nombre
-    
-    def to_dict(self):
-        return {'id_cargo': self._id, 'nombre': self._nombre}
